7d2d2b65dfead342362175a1d8ed1c606c02b2a9
MustoLini <danielssonjohannes@gmail.com>
2023-01-12 10:32:05 +0100

    Just fixed small thing

--- app/src/test/java/se/iths/AppTest.java
+++ app/src/test/java/se/iths/AppTest.java
@@ -1,10 +1,13 @@
 package se.iths;
 
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void appHasAGreeting() throws SQLException {
         App classUnderTest = new App();
     }
 }
